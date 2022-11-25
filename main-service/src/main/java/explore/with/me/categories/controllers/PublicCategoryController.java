cd4487ad14a40eb6a55247de5cf0034ae5605c88
Sevko Lev <booogiman@yandex.ru>
2022-11-26 02:49:33 +0300

    Дипломный проект dev 1981 - Доработка приложения по тестам

---
+++ main-service/src/main/java/explore/with/me/categories/controllers/PublicCategoryController.java
@@ -0,0 +1,33 @@
+package explore.with.me.categories.controllers;
+
+import explore.with.me.categories.dto.CategoryDto;
+import explore.with.me.categories.services.PublicCategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.Collection;
+
+@RestController
+@RequestMapping(path = "/categories")
+@Validated
+@RequiredArgsConstructor
+public class PublicCategoryController {
+
+    private final PublicCategoryService publicCategoryService;
+
+    @GetMapping
+    public Collection<CategoryDto> getCategories(@RequestParam(name = "from", defaultValue = "0")
+                                                 @PositiveOrZero Integer from,
+                                                 @RequestParam(name = "size", defaultValue = "10")
+                                                 @Positive Integer size) {
+        return publicCategoryService.getCategories(from, size);
+    }
+
+    @GetMapping("/{catId}")
+    public CategoryDto getCategoryById(@PathVariable @Positive Long catId) {
+        return publicCategoryService.getCategoryDtoById(catId);
+    }
+}
